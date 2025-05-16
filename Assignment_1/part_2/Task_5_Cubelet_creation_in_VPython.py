07d306b274bdb02745d03d0fefa4aaf1324015b5
SHUB-1806 <shubhammaheshjoshi@gmail.com>
2025-05-16 13:38:11 +0530

    minor changes

--- Assignment_1/part_2/Task_5_Cubelet_creation_in_VPython.py
+++ Assignment_1/part_2/Task_5_Cubelet_creation_in_VPython.py
@@ -8,4 +8,5 @@ for x in [-1, 0, 1]:
             box(pos=vector(x*spacing, y*spacing, z*spacing), size=vector(1,1,1), color=color.white)
 
 while True:
-    rate(30) 
+    pass
+# The above code creates a 3D grid of cubes in VPython, representing the cubelets of a Rubik's Cube.
